326c604ec1a577def80d113ed47a92a6315a47b4
nikita03565 <33401112+nikita03565@users.noreply.github.com>
2018-12-16 03:38:26 +0300

    initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_zulKPDxM6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '34v4l0b5djh8chrq2s1u92mee7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
