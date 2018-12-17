8e30052c0af63a3c5b4a0e9845524deffb5013ee
nikita03565 <33401112+nikita03565@users.noreply.github.com>
2018-12-17 18:35:59 +0300

    somehow adding persons to db works

--- js/addperson.js
+++ js/addperson.js
@@ -1,32 +1,28 @@
-/*global WildRydes _config*/
+/*global webcourses _config*/
 
-var WildRydes = window.WildRydes || {};
-WildRydes.map = WildRydes.map || {};
-
+var webcourses = window.webcourses || {};
+webcourses.map = webcourses.map || {};
 (function rideScopeWrapper($) {
     var authToken;
-    WildRydes.authToken.then(function setAuthToken(token) {
+    webcourses.authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
         } else {
-            window.location.href = '/signin.html';
+            window.location.href = 'signin.html';
         }
     }).catch(function handleTokenError(error) {
         alert(error);
-        window.location.href = '/signin.html';
+        window.location.href = 'signin.html';
     });
-    function requestUnicorn(pickupLocation) {
+    function AddPerson() {
         $.ajax({
             method: 'POST',
-            url: _config.api.invokeUrl + '/ride',
+            url: _config.api.invokeUrl + '/people',
             headers: {
                 Authorization: authToken
             },
             data: JSON.stringify({
-                PickupLocation: {
-                    Latitude: pickupLocation.latitude,
-                    Longitude: pickupLocation.longitude
-                }
+                Amount: Math.random()
             }),
             contentType: 'application/json',
             success: completeRequest,
@@ -47,7 +43,7 @@ WildRydes.map = WildRydes.map || {};
         displayUpdate(unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way.');
         animateArrival(function animateCallback() {
             displayUpdate(unicorn.Name + ' has arrived. Giddy up!');
-            WildRydes.map.unsetLocation();
+            webcourses.map.unsetLocation();
             $('#request').prop('disabled', 'disabled');
             $('#request').text('Set Pickup');
         });
@@ -57,13 +53,13 @@ WildRydes.map = WildRydes.map || {};
     $(function onDocReady() {
         $('#request').click(handleRequestClick);
         $('#signOut').click(function() {
-            WildRydes.signOut();
+            webcourses.signOut();
             alert("You have been signed out.");
             window.location = "signin.html";
         });
-        $(WildRydes.map).on('pickupChange', handlePickupChanged);
+        $(webcourses.map).on('pickupChange', handlePickupChanged);
 
-        WildRydes.authToken.then(function updateAuthMessage(token) {
+        webcourses.authToken.then(function updateAuthMessage(token) {
             if (token) {
                 displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
                 $('.authToken').text(token);
@@ -82,28 +78,9 @@ WildRydes.map = WildRydes.map || {};
     }
 
     function handleRequestClick(event) {
-        var pickupLocation = WildRydes.map.selectedPoint;
+        var pickupLocation = webcourses.map.selectedPoint;
         event.preventDefault();
         requestUnicorn(pickupLocation);
-    }
-
-    function animateArrival(callback) {
-        var dest = WildRydes.map.selectedPoint;
-        var origin = {};
-
-        if (dest.latitude > WildRydes.map.center.latitude) {
-            origin.latitude = WildRydes.map.extent.minLat;
-        } else {
-            origin.latitude = WildRydes.map.extent.maxLat;
-        }
-
-        if (dest.longitude > WildRydes.map.center.longitude) {
-            origin.longitude = WildRydes.map.extent.minLng;
-        } else {
-            origin.longitude = WildRydes.map.extent.maxLng;
-        }
-
-        WildRydes.map.animate(origin, dest, callback);
     }
 
     function displayUpdate(text) {
